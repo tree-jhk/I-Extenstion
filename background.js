b26314a574972ff63ed5a669750edfe96842702c
Inha <email>
2024-05-01 23:18:07 +0900

    Feat: Add custom design

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
-// background.js
-chrome.runtime.onInstalled.addListener(() => {
-  // 설치 시 필요한 초기 설정
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "sendMessage") {
+    const message = request.message;
+    // Process the message and generate a response using GPT-3.5 API
+    const answer = "This is a sample response from GPT-3.5 API.";
+    sendResponse({ answer: answer });
+  }
 });
--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,5 @@
 function addListItem() {
   const ul = document.getElementsByClassName("left-menus")[0];
-  console.log(ul);
   if (ul) {
     const newLi = document.createElement("li");
     newLi.setAttribute("id", "i-quiz-link");
@@ -47,9 +46,88 @@ function replaceMainDiv() {
   if (regionMain && regionMain.children.length > 1) {
     const secondChild = regionMain.children[1];
     const newDiv = document.createElement("div");
-    newDiv.className = "webview-container";
+    newDiv.className = "chat-container";
     newDiv.innerHTML = `
-      <iframe src="http://127.0.0.1:8080/" style="width: 100%; height: 100%; border: none;"></iframe>
+    <div class="chat-interface">
+    <div class="left-panel">
+      <h3>Customize Your Chat persona:)</h3>
+      <div class="option-group">
+        <h4>Demographic Cues</h4>
+        <label class="toggle-switch">
+          <input type="checkbox">
+          <span class="toggle-slider"></span>
+        </label>
+        <div class="verbal-style-group">
+          <label><input type="radio" name="demographic" value="yuri"> Yuri</label>
+          <label><input type="radio" name="demographic" value="no-gender"> No gender</label>
+        </div>
+      </div>
+      <div class="option-group">
+        <h4>Verbal Style Cues</h4>
+        <label class="toggle-switch">
+          <input type="checkbox">
+          <span class="toggle-slider"></span>
+        </label>
+        <div class="verbal-style-group">
+          <label><input type="radio" name="verbal-style" value="unpredictable"> Unpredictable</label>
+          <label><input type="radio" name="verbal-style" value="curious"> Curious</label>
+          <label><input type="radio" name="verbal-style" value="snarky"> Snarky</label>
+          <label><input type="radio" name="verbal-style" value="unusual"> Unusual</label>
+          <label><input type="radio" name="verbal-style" value="funny"> Funny</label>
+          <label><input type="radio" name="verbal-style" value="non-judgmental"> Non-judgmental</label>
+          <label><input type="radio" name="verbal-style" value="curious"> Curious</label>
+          <label><input type="radio" name="verbal-style" value="caring"> Caring</label>
+          <label><input type="radio" name="verbal-style" value="creative"> Creative</label>
+          <label><input type="radio" name="verbal-style" value="casual"> Casual</label>
+          <label><input type="radio" name="verbal-style" value="neurotic"> Neurotic</label>
+          <label><input type="radio" name="verbal-style" value="rational"> Rational</label>
+        </div>
+      </div>
+      <div class="option-group">
+        <h4>Emoji</h4>
+        <label class="toggle-switch">
+          <input type="checkbox">
+          <span class="toggle-slider"></span>
+        </label>
+      </div>
+      <div class="option-group">
+        <h4>Knowledge and Interest Cues</h4>
+        <label class="toggle-switch">
+          <input type="checkbox">
+          <span class="toggle-slider"></span>
+        </label>
+        <div class="verbal-style-group">
+          <label><input type="radio" name="knowledge" value="shamanism"> Shamanism</label>
+          <label><input type="radio" name="knowledge" value="psychology"> Psychology</label>
+          <label><input type="radio" name="knowledge" value="neuroscience-rights"> Neuroscience rights</label>
+        </div>
+      </div>
+      <div class="option-group">
+        <h4>Other settings</h4>
+        <label class="toggle-switch">
+          <input type="checkbox">
+          <span class="toggle-slider"></span>
+        </label>
+        <div class="verbal-style-group">
+          <label><input type="radio" name="other-settings" value="small-talk"> Small talk</label>
+          <label><input type="radio" name="other-settings" value="meta-relational-talk"> Meta-relational talk</label>
+          <label><input type="radio" name="other-settings" value="humor"> Humor</label>
+          <label><input type="radio" name="other-settings" value="greeting"> Greeting</label>
+        </div>
+      </div>
+      <div class="option-group">
+        <h4>Appearance</h4>
+        <!-- Add appearance options here -->
+      </div>
+    </div>
+    <div class="right-panel">
+      <div id="messages"></div>
+      <div class="input-container">
+        <input type="text" id="message-input" placeholder="Type your message...">
+        <button id="sendButton">Send</button>
+      </div>
+    </div>
+  </div>
     `;
 
     newDiv.style.cssText = `
@@ -62,6 +140,30 @@ function replaceMainDiv() {
     `;
 
     regionMain.replaceChild(newDiv, secondChild);
+
+    // Add event listener for send button
+    const sendButton = document.getElementById("sendButton");
+    sendButton.addEventListener("click", sendMessage);
+  }
+}
+
+function sendMessage() {
+  const input = document.getElementById("message-input");
+  const message = input.value.trim();
+  if (message) {
+    const messagesContainer = document.getElementById("messages");
+    const messageDiv = document.createElement("div");
+    messageDiv.textContent = message;
+    messagesContainer.appendChild(messageDiv);
+    input.value = ""; // Clear the input field
+
+    // Send the message to the background script for processing
+    chrome.runtime.sendMessage({ type: "sendMessage", message: message }, (response) => {
+      const responseDiv = document.createElement("div");
+      responseDiv.textContent = response.answer;
+      messagesContainer.appendChild(responseDiv);
+      messagesContainer.scrollTop = messagesContainer.scrollHeight; // Scroll to the bottom
+    });
   }
 }
 
